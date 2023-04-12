eb4205c3dd25ec22f788cd406a66d2f99c61d3b7
Leticia Oliveira <leticia.oliveira201489@gmail.com>
2023-04-12 11:02:04 -0300

    Projeto Hello World

---
+++ src/main/java/com/generation/helloworldd/HelloworlddApplication.java
@@ -0,0 +1,13 @@
+package com.generation.helloworldd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworlddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworlddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/helloworldd/HelloworlddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.helloworldd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworlddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
