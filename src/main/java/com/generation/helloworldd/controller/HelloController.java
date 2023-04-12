78421f6a1329a681091005b5fbd4f40a1722a1c2
Leticia Oliveira <leticia.oliveira201489@gmail.com>
2023-04-12 11:21:03 -0300

    Web BSMs

--- src/main/java/com/generation/helloworldd/controller/HelloController.java
+++ src/main/java/com/generation/helloworldd/controller/HelloController.java
@@ -12,4 +12,14 @@ public class HelloController {
 	public String hello() {
 		return "<b>Hello, World!</b>";
 	}
+	
+	@GetMapping("/bsms")
+	public String bsms() {
+		return "<b>BSMs da Generation: /-Responsabilidade pessoal; /-Mentalidade de crescimento; /-Orientação ao futuro; /-Persistência; /-Comunicação; /-Orientação aos detalhes; /-Trabalho em equipe; /-Proatividade.";
+	}
+	
+	@GetMapping("/obaps")
+	public String obaps() {
+		return "<b>Objetivos de aprendizagem da semana:<br /Meus objetivos de aprendizagem para esta semana são: recuperar as lacunas que ficaram da semana passada, deixar a matéria dessa semana em dia e estudar por fora, além de aplicar meu conhecimento em projetos.</b>";
+	}
 }
